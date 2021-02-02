adc1eb083634744f6362aaaa30fade8925955703
Sirine Dridi <sirine@live.unc.edu>
2021-02-02 09:37:07 -0500

    Update A1Example.java

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class A1Example {
 
 	public static void main(String[] args) {
-		// Print out the Java version number, just to let 
-		// you know what Eclipse thinks it is using and
-		// so that you can confirm that it is 10.0 or higher.
 		
-		System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
 		// Set up scanner object to read space-separated values
 		// from console input
