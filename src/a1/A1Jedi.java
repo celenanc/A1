5d69f163c0373550f052b08726b4e44bb28bcb7b
Celena Case <celenanc@live.unc.edu>
2021-02-10 23:37:20 -0500

    A1Jedi Complete

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,30 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int inventory = scan.nextInt();
 		
+		String[] offeredI = new String[inventory];
+		double[] cost = new double[inventory];
+		double[] item = new double[inventory];
+		String[] name = new String[inventory];
+				
+		for (int i = 0; i < inventory; i++) {
+			offeredI[i] = scan.next();
+			cost[i] = scan.nextDouble();
+		}
+		
+		int person = scan.nextInt();
+		
+		for (int i = 0; i < person; i++) {
+			String fullName = scan.next() + scan.next();
+			name[i] = fullName;
+			int diffTypes = scan.nextInt();
+			double total = 0;
+			for (int j = 0; j < diffTypes; j++) {
+				int all = scan.nextInt();
+				total += all * findItemPrice(scan.next(), offeredI, cost);
+			}
+			item[i] = total;
+		}
 	}
 }
